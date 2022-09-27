b71830de5612fa25a5003b9f371e457c0497b127
Matt Cavalier <matthewjcavalier@pm.me>
2022-09-27 18:22:44 -0500

    finished ch 5

--- src/main/kotlin/tool/GenerateAst.kt
+++ src/main/kotlin/tool/GenerateAst.kt
@@ -1,6 +1,7 @@
 package tool
 
 import java.io.File
+import java.util.*
 import kotlin.system.exitProcess
 
 val EXPR = "Expression"
@@ -28,7 +29,10 @@ fun defineAst(outputDir: String, baseName: String, types: List<String>) {
 
         write("package klox")
         write("")
-        write("abstract class $baseName()")
+        write("abstract class $baseName() {")
+        defineVisitor(write, baseName, types)
+        write("abstract fun <R> accept(visitor: Visitor<R>): R")
+        write("}")
         types.forEach { type ->
             val split = type.split("->").map { it.trim() }
             val className = split[0]
@@ -36,6 +40,15 @@ fun defineAst(outputDir: String, baseName: String, types: List<String>) {
             defineType(write, baseName, className, fields)
         }
     }
+}
+
+fun defineVisitor(write: StringConsumer, baseName: String, types: List<String>) {
+    write("interface Visitor<R> {")
+    types.forEach { type ->
+        val typeName = type.split("->").first().trim()
+        write("fun visit$typeName$baseName(${baseName.lowercase(Locale.getDefault())}: $typeName): R")
+    }
+    write("}")
 }
 
 fun defineType(
@@ -47,8 +60,11 @@ fun defineType(
     val fields = fieldList.split(",")
         .map { it.trim() }
         .map { "val $it,\n" }
-        .foldRight("") { s: String, acc: String -> "$acc$s" }
+        .reversed()
+        .fold("") { s: String, acc: String -> "$acc$s" }
 
-    writer("data class $className($fields): $baseName()")
+    writer("class $className($fields): $baseName() {")
+    writer("override fun <R> accept(visitor: Visitor<R>): R { return visitor.visit$className$baseName(this)}")
+    writer("}")
 
 }
